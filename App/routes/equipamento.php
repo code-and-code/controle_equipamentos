f18b4bccc75e0b1057e4ef06d21e85bd86761176
Angelo Biscola <angelo.biscola@gmail.com>
2017-07-10 17:05:47 -0300

    Fix Auth route

--- App/routes/equipamento.php
+++ App/routes/equipamento.php
@@ -3,10 +3,10 @@
 $prefix     = 'equipamento';
 $controller = 'EquipamentoController';
 
-$ar["{$prefix}.index"]     = ['route' => "/{$prefix}",          'controller' => $controller, 'method'=>'GET',  'action' => 'index'   ];
-$ar["{$prefix}.create"]     = ['route' => "/{$prefix}/novo",       'controller' => $controller, 'method'=>'GET',  'action' => 'create'   ];
-$ar["{$prefix}.store"]     = ['route' => "/{$prefix}/gravar",       'controller' => $controller, 'method'=>'POST',  'action' => 'store'   ];
-$ar["{$prefix}.edit"]     = ['route' => "/{$prefix}/editar",       'controller' => $controller, 'method'=>'GET',  'action' => 'edit'   ];
-$ar["{$prefix}.update"]     = ['route' => "/{$prefix}/atualizar",       'controller' => $controller, 'method'=>'POST',  'action' => 'update'   ];
-$ar["{$prefix}.delete"]     = ['route' => "/{$prefix}/deletar",       'controller' => $controller, 'method'=>'GET',  'action' => 'delete'   ];
+$ar["{$prefix}.index"]    = ['route' => "/{$prefix}",          'controller' => $controller, 'method'=>'GET',  'auth' =>true,    'action' => 'index'    ];
+$ar["{$prefix}.create"]   = ['route' => "/{$prefix}/novo",     'controller' => $controller, 'method'=>'GET',  'auth' =>true,    'action' => 'create'   ];
+$ar["{$prefix}.store"]    = ['route' => "/{$prefix}/gravar",   'controller' => $controller, 'method'=>'POST', 'auth' =>true,    'action' => 'store'    ];
+$ar["{$prefix}.edit"]     = ['route' => "/{$prefix}/editar",   'controller' => $controller, 'method'=>'GET',  'auth' =>true,    'action' => 'edit'     ];
+$ar["{$prefix}.update"]   = ['route' => "/{$prefix}/atualizar",'controller' => $controller, 'method'=>'POST', 'auth' =>true,    'action' => 'update'   ];
+$ar["{$prefix}.delete"]   = ['route' => "/{$prefix}/deletar",  'controller' => $controller, 'method'=>'GET',  'auth' =>true,    'action' => 'delete'   ];
 
--- App/routes/licenca.php
+++ App/routes/licenca.php
@@ -3,17 +3,17 @@
 $prefix     = 'licenca';
 $controller = 'LicencaController';
 
-$ar["{$prefix}.index"]     = ['route' => "/{$prefix}",          'controller' => $controller, 'method'=>'GET',  'action' => 'index'   ];
-$ar["{$prefix}.create"]     = ['route' => "/{$prefix}/novo",       'controller' => $controller, 'method'=>'GET',  'action' => 'create'   ];
-$ar["{$prefix}.store"]     = ['route' => "/{$prefix}/gravar",       'controller' => $controller, 'method'=>'POST',  'action' => 'store'   ];
-$ar["{$prefix}.edit"]     = ['route' => "/{$prefix}/editar",       'controller' => $controller, 'method'=>'GET',  'action' => 'edit'   ];
-$ar["{$prefix}.update"]     = ['route' => "/{$prefix}/atualizar",       'controller' => $controller, 'method'=>'POST',  'action' => 'update'   ];
-$ar["{$prefix}.delete"]     = ['route' => "/{$prefix}/deletar",       'controller' => $controller, 'method'=>'GET',  'action' => 'delete'   ];
+$ar["{$prefix}.index"]     = ['route' => "/{$prefix}",           'controller' => $controller, 'method'=>'GET',  'auth' =>true,  'action' => 'index'    ];
+$ar["{$prefix}.create"]    = ['route' => "/{$prefix}/novo",      'controller' => $controller, 'method'=>'GET',  'auth' =>true,  'action' => 'create'   ];
+$ar["{$prefix}.store"]     = ['route' => "/{$prefix}/gravar",    'controller' => $controller, 'method'=>'POST', 'auth' =>true,  'action' => 'store'    ];
+$ar["{$prefix}.edit"]      = ['route' => "/{$prefix}/editar",    'controller' => $controller, 'method'=>'GET',  'auth' =>true,  'action' => 'edit'     ];
+$ar["{$prefix}.update"]    = ['route' => "/{$prefix}/atualizar", 'controller' => $controller, 'method'=>'POST', 'auth' =>true,  'action' => 'update'   ];
+$ar["{$prefix}.delete"]    = ['route' => "/{$prefix}/deletar",   'controller' => $controller, 'method'=>'GET',  'auth' =>true,  'action' => 'delete'   ];
 
 
-$ar["{$prefix}.serial.search_licenca"]  = ['route' => "/{$prefix}/serial/por_licenca",  'controller' => 'SerialController', 'method'=>'GET', 'action' => 'findByLicenca'];
-$ar["{$prefix}.serial.create"]          = ['route' => "/{$prefix}/serial/novo",         'controller' => 'SerialController', 'method'=>'GET', 'action' => 'create'  ];
-$ar["{$prefix}.serial.store"]           = ['route' => "/{$prefix}/serial/gravar",       'controller' => 'SerialController', 'method'=>'POST','action' => 'store'   ];
-$ar["{$prefix}.serial.edit"]            = ['route' => "/{$prefix}/serial/editar",       'controller' => 'SerialController', 'method'=>'GET', 'action' => 'edit'    ];
-$ar["{$prefix}.serial.update"]          = ['route' => "/{$prefix}/serial/atualizar",    'controller' => 'SerialController', 'method'=>'POST','action' => 'update'  ];
-$ar["{$prefix}.serial.delete"]          = ['route' => "/{$prefix}/serial/deletar",      'controller' => 'SerialController', 'method'=>'GET', 'action' => 'delete'  ];
+$ar["{$prefix}.serial.search_licenca"]  = ['route' => "/{$prefix}/serial/por_licenca",  'controller' => 'SerialController', 'method'=>'GET', 'auth' =>true, 'action' => 'findByLicenca'];
+$ar["{$prefix}.serial.create"]          = ['route' => "/{$prefix}/serial/novo",         'controller' => 'SerialController', 'method'=>'GET', 'auth' =>true, 'action' => 'create'  ];
+$ar["{$prefix}.serial.store"]           = ['route' => "/{$prefix}/serial/gravar",       'controller' => 'SerialController', 'method'=>'POST','auth' =>true, 'action' => 'store'   ];
+$ar["{$prefix}.serial.edit"]            = ['route' => "/{$prefix}/serial/editar",       'controller' => 'SerialController', 'method'=>'GET', 'auth' =>true, 'action' => 'edit'    ];
+$ar["{$prefix}.serial.update"]          = ['route' => "/{$prefix}/serial/atualizar",    'controller' => 'SerialController', 'method'=>'POST','auth' =>true, 'action' => 'update'  ];
+$ar["{$prefix}.serial.delete"]          = ['route' => "/{$prefix}/serial/deletar",      'controller' => 'SerialController', 'method'=>'GET', 'auth' =>true, 'action' => 'delete'  ];
