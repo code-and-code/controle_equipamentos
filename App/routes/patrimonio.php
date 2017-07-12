1cb98f7f6922e1e59f8e5d3a710948c2f9cefc37
Adriano <adriano_vianna@outlook.com>
2017-07-12 13:53:46 -0300

    update fix

--- App/routes/patrimonio.php
+++ App/routes/patrimonio.php
@@ -4,13 +4,8 @@ $prefix     = 'patrimonio';
 $controller = 'PatrimonioController';
 
 $ar["{$prefix}.index"]     = ['route' => "/{$prefix}",          'controller' => $controller, 'method'=>'GET',  'action' => 'index'   ];
-$ar["{$prefix}.create"]     = ['route' => "/{$prefix}/novo",       'controller' => $controller, 'method'=>'GET',  'action' => 'create'   ];
+/*$ar["{$prefix}.create"]     = ['route' => "/{$prefix}/novo",       'controller' => $controller, 'method'=>'GET',  'action' => 'create'   ];
 $ar["{$prefix}.store"]     = ['route' => "/{$prefix}/gravar",       'controller' => $controller, 'method'=>'POST',  'action' => 'store'   ];
 $ar["{$prefix}.edit"]     = ['route' => "/{$prefix}/editar",       'controller' => $controller, 'method'=>'GET',  'action' => 'edit'   ];
 $ar["{$prefix}.update"]     = ['route' => "/{$prefix}/atualizar",       'controller' => $controller, 'method'=>'POST',  'action' => 'update'   ];
-$ar["{$prefix}.delete"]     = ['route' => "/{$prefix}/deletar",       'controller' => $controller, 'method'=>'GET',  'action' => 'delete'   ];
-
-/*
-$ar['auth.login']     = ['route' => '/auth/login',    'controller' => 'AuthController', 'method'=>'POST', 'action' => 'postLogin'  ];
-$ar['auth.logout']    = ['route' => '/auth/logout',   'controller' => 'AuthController', 'method'=>'GET',  'action' => 'logout'     ];
-*/
+$ar["{$prefix}.delete"]     = ['route' => "/{$prefix}/deletar",       'controller' => $controller, 'method'=>'GET',  'action' => 'delete'   ];*/
