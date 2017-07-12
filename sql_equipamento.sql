090c5401fc435eb8aee8b90616de7c0dfc849f73
Adriano <adriano_vianna@outlook.com>
2017-07-12 14:39:35 -0300

    update fix

--- sql_equipamento.sql
+++ sql_equipamento.sql
@@ -150,16 +150,13 @@ CREATE TABLE `patrimonios` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `num_patrimonial` varchar(255) DEFAULT NULL,
   `serial` varchar(255) DEFAULT NULL,
-  `categoria_id` int(11) unsigned NOT NULL,
   `local_id` int(11) unsigned NOT NULL,
   `funcionario_id` int(11) unsigned NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `patrimonio_categoria_id_foreign` (`categoria_id`),
   KEY `patrimonio_local_id_foreign` (`local_id`),
   KEY `patrimonio_funcionario_id_foreign` (`funcionario_id`),
-  CONSTRAINT `patrimonio_categoria_id_foreign` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE CASCADE,
   CONSTRAINT `patrimonio_funcionario_id_foreign` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionarios` (`id`) ON DELETE CASCADE,
   CONSTRAINT `patrimonio_local_id_foreign` FOREIGN KEY (`local_id`) REFERENCES `locais` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
