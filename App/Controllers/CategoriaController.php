0d88346f757a0be56d5f36c4c3859dbbe4a72094
Adriano <adriano_vianna@outlook.com>
2017-08-17 13:19:36 -0300

    fix category

--- App/Controllers/CategoriaController.php
+++ App/Controllers/CategoriaController.php
@@ -2,18 +2,20 @@
 namespace App\Controllers;
 
 use App\Models\Categoria;
-use App\Support\CrudControllerTrait;
 use Cac\Controller\Action;
 
 class CategoriaController extends Action
 {
-    public $model;
-
-    use CrudControllerTrait;
+    private $model;
 
     public function __construct()
     {
         $this->model = new Categoria();
+    }
+
+    public function index()
+    {
+        echo $this->render("categoria.index", ['results' => $this->model->all()]);
     }
 
 }
--- App/Models/Categoria.php
+++ App/Models/Categoria.php
@@ -5,5 +5,5 @@ namespace App\Models;
 class Categoria extends Model {
 
     protected $table      = "categorias";
-    public    $requested  = ['nome'];
+
 }
